33038d4d62de39313663868e65c300070762ac90
Luke Kim <luke3874@vt.edu>
2019-12-01 20:08:16 -0500

    hey

--- data_clean.R
+++ data_clean.R
@@ -5,3 +5,17 @@ boston_data <- subset(boston_data, boston_data$YEAR == 2018)
 boston_data <- boston_data[,-7]
 
 write.csv(boston_data, "./boston_data.csv")
+
+#for 6 specific data
+library(dplyr)
+offenses = c("Motor Vehicle Accident Response",
+             "Drug Violation",
+             "Larceny",
+             "Aggravated Assault",
+             "Auto Theft",
+             "Residential Burglary")
+new_data = boston_data %>% filter(boston_data$OFFENSE_CODE_GROUP == offenses)
+
+write.csv(new_data, "./topSixCrimes.csv")
+
+
