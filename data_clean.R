1d4b0259887c432a6561485d79fa4ea9ed787fa1
mitcs2018 <mneat21@vt.edu>
2019-12-01 19:12:57 -0500

    cleaning data and cutting to 2018 only

--- data_clean.R
+++ data_clean.R
@@ -1,1 +1,7 @@
 #clean data here
+
+boston_data <- read.csv("boston_data.csv")
+boston_data <- subset(boston_data, boston_data$YEAR == 2018)
+boston_data <- boston_data[,-7]
+
+write.csv(boston_data, "./boston_data.csv")
